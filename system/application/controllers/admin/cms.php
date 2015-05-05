ec400a55925cbc871b20103aea26bdc58e16af3d
Kaushtuv <kaushtuvgurung@gmail.com>
2015-05-05 15:42:53 +1000

    Updates

--- system/application/controllers/admin/cms.php
+++ system/application/controllers/admin/cms.php
@@ -3100,10 +3100,10 @@ class Cms extends Controller {
 			if ($this->Content_model->add_banner($banner)) {
 				# Create thumbnails
 				
-				#$this->advanced_resizephoto($data,"./uploads/banners/","thumb",$width,$height,315,117);
+				$this->advanced_resizephoto($data,"./uploads/banners/","thumb",$width,$height,315,117);
 				#$this->resizephotobig($data,"./uploads/banners/ori","thumb",$width,$height,1200,600);
 				#$this->resizephotobig2($data,"./uploads/banners/ori2","thumb",$width,$height,1200,920);
-				#$this->resizephotobig($data,"./uploads/banners/ori","thumb",$width,$height,1912,869);
+				$this->resizephotobig($data,"./uploads/banners/ori","thumb",$width,$height,1912,869);
 				$this->resizephotobig2($data,"./uploads/banners/ori2","thumb",$width,$height,1912,869);
 				//$this->advanced_resizephoto($data,"./uploads/banners/","thumb",$width,$height,800,600);
 				//$this->resizephoto($name,"./uploads/banners","thumb",315,117);			
