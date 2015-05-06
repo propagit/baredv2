f11f62957cff8dab530b386543290d40d61a009f
Kaushtuv <kaushtuvgurung@gmail.com>
2015-05-06 13:18:10 +1000

    Home Page Update

--- system/application/views/home/main_view.php
+++ system/application/views/home/main_view.php
@@ -1,5 +1,7 @@
 <?php $this->load->view('home/common/banners'); ?>
 <?php $this->load->view('home/common/tiles');?>
+<?php if($feature_products){ ?>
 <div class="app-container relative bar bg-black text-white"><h3>our latest range</h3></div>
 <?php $this->load->view('common/products/featured_products'); ?>
+<?php } ?>
 <?php $this->load->view('home/common/instagram_carousel'); ?>
